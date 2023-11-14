8e4d02dae7f2d0d012d672ba36c2377713de865a
NSMatthew <matthewnovansidharta31@gmail.com>
2023-11-14 13:46:22 +0800

    menghubungkan add php dengan css ext

--- add.php
+++ add.php
@@ -1,31 +1,42 @@
 <html>
 <head>
 	<title>Add Users</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="eksternal.css">
+    <title>Action add php</title>
 </head>
  
 <body>
-	<a href="index.php">Go to Home</a>
+	<a href="index.php">Ke Index php</a>
 	<br/><br/>
  
 	<form action="add.php" method="post" name="form1">
 		<table width="25%";border="0">
 			<tr> 
-				<td>Name</td>
-				<td><input type="text" name="name"></td>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+            <tr> 
+				<td>ID</td>
+				<td><input type="number" name="id"></td>
 			</tr>
 			<tr> 
 				<td>Email</td>
 				<td><input type="text" name="email"></td>
 			</tr>
 			<tr> 
-				<td>Mobile</td>
-				<td><input type="text" name="mobile"></td>
-			</tr>
-			<tr> 
-				<td></td>
-				<td><input type="submit" name="Submit" value="Add"></td>
+				<td>Status</td>
+				<td>
+                <select name="statusUser">
+                <option value="aktif">Aktif</option>
+                <option value="tidak aktif">Tidak Aktif</option>
+            </select>
+                </td>
 			</tr>
 		</table>
+        <button class="btn btn-primary" type="submit"><font5>Tambah</font5></button>
 	</form>
 	
 	<?php
@@ -41,10 +52,10 @@
 		include_once("koneksi.php");
 				
 		// Insert user data into table
-		$result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
+		$result = mysqli_query($mysqli, "INSERT INTO user_admin(nama,id,email,statusUser) VALUES('$nama','$id','$email','statusUser')");
 		
 		// Show message when user added
-		echo "User added successfully. <a href='index.php'>View Users</a>";
+		echo "Data berhasil ditambahkan. <a href='index.php'>View Users</a>";
 	}
 	?>
 </body>
