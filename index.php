9b61da2442294a124c4e76f8a0acd07d0422323e
NSMatthew <matthewnovansidharta31@gmail.com>
2023-11-14 13:27:25 +0800

    nambah file index dan add php, ubah form.html

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+//memasukkan koneksi ke sini
+include_once("koneksi.php");
+
+$result = mysqli_query(mysqli, "SELECT * FROM user_admin ORDER BY id ASC")
+
+?>
+
+<html>
+    <head>
+        <title>Homepage</title>
+    </head>
+
+    <body>
+
+    <a> </a>
+
+    </body>
+
+</html>
--- koneksi.php
+++ koneksi.php
@@ -13,18 +13,3 @@ if (!$conn) {
 }
 echo "Koneksi Berhasil!";
 ?>
-
-<?php
-
-$query = "INSERT INTO user_admin (nama, id, email, statusUser) VALUES (?, ?, ?, ?)";
-$stmt = mysqli_prepare($conn, $query);
-mysqli_stmt_bind_param($stmt, "ssss", $nama, $id, $email, $status);
-
-$nama = $_POST['nama'];
-$id = $_POST['id'];
-$email = $_POST['email'];
-$status = $_POST['statusUser'];
-
-mysqli_stmt_execute($stmt);
-
-?>
